2e7a13535c964fb07a1b2727ae8a4c57c71620d9
Hill <hill@kimo.com>
2021-05-19 02:27:59 +0800

    add testPathToUrlWithPort

--- tests/PathToUrlTest.php
+++ tests/PathToUrlTest.php
@@ -21,6 +21,18 @@ class PathToUrlTest extends TestCase
         $this->assertEquals($expected,$actural);
     }
 
+
+    public function testPathToUrlWithPort() {
+        $path = '/error';
+        $p = \PMVC\plug($this->_plug);
+        $p['REQUEST_URI'] = 'http://xxx:8888/index.php'; 
+        $p['SCRIPT_NAME'] = '/index.php';
+        \PMVC\option('set','realUrl',$p->realUrl());
+        $expected = 'http://xxx:8888/index.php/error';
+        $actural = $p->pathToUrl($path);
+        $this->assertEquals($expected,$actural);
+    }
+
     public function testUrlToUrl()
     {
         $path = 'http://xxx/index.php/error';
